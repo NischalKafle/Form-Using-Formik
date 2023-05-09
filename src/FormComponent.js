37571701b46de59f868f21983cfe20f046b8b33c
Nischal <kafle.nischal2021@gmail.com>
2023-05-09 15:18:27 +0545

    Handling Form Submission

---
+++ src/FormComponent.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {useFormik} from 'formik';
+function FormComponent() {
+    const formik = useFormik ({
+        initialValues:{
+        name: 'Raj',
+        email: '',
+        age:0 ,
+      },
+      onSubmit:values=>{
+console.log('Submitted Values:',values)
+      }
+    }
+    )
+    
+      
+     
+    
+      
+return (
+    
+ <div>
+    <h1>Form </h1>
+    <form id="main" onSubmit={formik.handleSubmit}>
+      <div id="txt">
+        <label id="lab" HTMLfor="name">Name:</label>
+        <input type="text" id="name" name="name" className="inp" onChange={formik.handleChange} value={formik.values.name} />
+      </div>
+      <div id="txt">
+              <label id="lab" HTMLfor="email">E-mail:</label>
+      <input type="email" id="email" name="email" className="inp" onChange={formik.handleChange} value={formik.values.email} />
+    </div>
+ <div id="txt">
+          <label id="lab" HTMLfor="age">Age:</label>
+      <input type="number" id="age" name="age" className="inp" onChange={formik.handleChange} value={formik.values.age} />
+    </div>
+ 
+      <button type="submit">Sumbit</button>
+      </form>
+    </div>
+
+   )
+}   
+
+
+export default FormComponent;
